d8d269a9e7a2fd335ca6c81eecb65f7f5eda6524
CleoY <ccyau@cpp.edu>
2021-12-05 05:17:57 +0000

    Finished code for entering raw mode.

--- kilo.c
+++ kilo.c
@@ -1,28 +1,58 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <errno.h>
 
 struct termios orig_termios;
 
 void enableRawMode();
 void disableRawMode();
+void die(const char *s);
 
 int main(void){
-    enableRawMode();
-
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-    return 0;
+  enableRawMode();
+  char c;
+  while (1) {
+    c = '\0';
+    if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) 
+      die("read");
+    if (iscntrl(c)) {
+      printf("%d\r\n", c);
+    } else {
+      printf("%d ('%c')\r\n", c, c);
+    }
+    if (c == 'q') 
+      break;
+  }
+  
+  return 0;
 }
 
 void enableRawMode(void) {
-  tcgetattr(STDIN_FILENO, &orig_termios);
+  if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) 
+    die("tcgetattr");
   atexit(disableRawMode);
+
   struct termios raw = orig_termios;
-  raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+  raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+  raw.c_oflag &= ~(OPOST);
+  raw.c_cflag |= (CS8);
+  raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+  raw.c_cc[VMIN] = 0;
+  raw.c_cc[VTIME] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) 
+    die("tcsetattr");
 }
 
 void disableRawMode() {
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+  if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    die("tcsetattr");
+}
+
+void die(const char *s) {
+  perror(s);
+  exit(1);
 }
