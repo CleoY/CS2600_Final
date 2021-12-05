9b87107ad175f5a714eeded0c94424fdd8d5357d
CleoY <ccyau@cpp.edu>
2021-12-05 03:55:42 +0000

    Added disableRaw() function.

--- kilo.c
+++ kilo.c
@@ -1,3 +1,28 @@
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void enableRawMode();
+void disableRawMode();
+
 int main(void){
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
+}
+
+void enableRawMode(void) {
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disableRawMode() {
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
