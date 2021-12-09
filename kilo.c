b0afd2f12ffdf83354a3d24f86586ca40380369d
CleoY <ccyau@cpp.edu>
2021-12-09 01:50:12 +0000

    No more errors, finished step 34

--- kilo.c
+++ kilo.c
@@ -5,11 +5,16 @@
 #include <stdio.h>
 #include <errno.h>
 #include <sys/ioctl.h>
-#include <string.h>
 
 #define CTRL_KEY(k) ((k) & 0x1f)
-#define ABUF_INIT {NULL, 0}
-#define KILO_VERSION "0.0.1"
+
+struct termios orig_termios;
+struct editorConfig {
+  int screenrows;
+  int screencols;
+  struct termios orig_termios;
+};
+struct editorConfig E;
 
 void enableRawMode();
 void disableRawMode();
@@ -21,21 +26,6 @@ void editorDrawRows();
 int getWindowSize(int *rows, int *cols);
 void initEditor();
 int getCursorPosition(int *rows, int *cols);
-void abAppend(struct abuf *ab, const char *s, int len);
-void abFree(struct abuf *ab);
-
-struct termios orig_termios;
-struct editorConfig {
-  int screenrows;
-  int screencols;
-  struct termios orig_termios;
-};
-struct editorConfig E;
-
-struct abuf {
-  char *b;
-  int len;
-};
 
 void initEditor() {
   if (getWindowSize(&E.screenrows, &E.screencols) == -1) 
@@ -53,56 +43,6 @@ int main(void){
   
   return 0;
 }
-
-//INPUT FUNCTION
-void editorProcessKeypress() {
-  char c = editorReadKey();
-  switch (c) {
-    case CTRL_KEY('q'):
-      write(STDOUT_FILENO, "\x1b[2J", 4);
-      write(STDOUT_FILENO, "\x1b[H", 3);
-      exit(0);
-      break;
-  }
-}
-
- 
-//OUTPUT FUNCTIONS
-void editorRefreshScreen() {
-  struct abuf ab = ABUF_INIT;
-
-  abAppend(&ab, "\x1b[?25l", 6);
-  abAppend(&ab, "\x1b[H", 3);
-  
-  editorDrawRows(&ab);
-  
-  abAppend(&ab, "\x1b[H", 3);
-  abAppend(&ab, "\x1b[?25h", 6);
-
-  write(STDOUT_FILENO, ab.b, ab.len);
-  abFree(&ab);
-}
-
-void editorDrawRows(struct abuf *ab) {
-  int y;
-  for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-        "Kilo editor -- version %s", KILO_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
-      abAppend(ab, welcome, welcomelen);
-    } else {
-      abAppend(ab, "~", 1);
-    }
-
-    abAppend(ab, "\x1b[K", 3);
-    if (y < E.screenrows - 1) {
-      abAppend(ab, "\r\n", 2);
-    }
-  }
-}
-
 
 // TERMINAL FUNCTIONS
 void enableRawMode(void) {
@@ -159,31 +99,49 @@ int getWindowSize(int *rows, int *cols) {
 int getCursorPosition(int *rows, int *cols) {
   char buf[32];
   unsigned int i = 0;
-  
-  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+  if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) 
+    return -1;
   while (i < sizeof(buf) - 1) {
-    if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
-    if (buf[i] == 'R') break;
+    if (read(STDIN_FILENO, &buf[i], 1) != 1) 
+      break;
+    if (buf[i] == 'R') 
+      break;
     i++;
   }
-  
   buf[i] = '\0';
   if (buf[0] != '\x1b' || buf[1] != '[') 
     return -1;
   if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) 
     return -1;
-  
   return 0;
 }
 
-//APPEND BUFFER
-void abAppend(struct abuf *ab, const char *s, int len) {
-  char *new = realloc(ab->b, ab->len + len);
-  if (new == NULL) return;
-  memcpy(&new[ab->len], s, len);
-  ab->b = new;
-  ab->len += len;
+//INPUT FUNCTION
+void editorProcessKeypress() {
+  char c = editorReadKey();
+  switch (c) {
+    case CTRL_KEY('q'):
+      write(STDOUT_FILENO, "\x1b[2J", 4);
+      write(STDOUT_FILENO, "\x1b[H", 3);
+      exit(0);
+      break;
+  }
 }
-void abFree(struct abuf *ab) {
-  free(ab->b);
+
+
+//OUTPUT FUNCTIONS
+void editorRefreshScreen() {
+  write(STDOUT_FILENO, "\x1b[2J", 4);
+  write(STDOUT_FILENO, "\x1b[H", 3);
+  editorDrawRows();
+  write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+
+//DIF FROM STEP 35
+void editorDrawRows() {
+  int y;
+  for (y = 0; y < E.screenrows; y++) {
+    write(STDOUT_FILENO, "~\r\n", 3);
+  }
 }
