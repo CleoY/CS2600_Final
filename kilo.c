5d45be788ed2c0f8b9469ec34c3f4196bd8f11f2
CleoY <ccyau@cpp.edu>
2021-12-10 04:07:38 +0000

    Finished step 90.

--- kilo.c
+++ kilo.c
@@ -37,6 +37,7 @@ typedef struct erow {
 struct termios orig_termios;
 struct editorConfig {
   int cx, cy;
+  int rx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -77,6 +78,7 @@ void abFree(struct abuf *ab);
 //ROW
 void editorAppendRow(char *s, size_t len);
 void editorUpdateRow(erow *row);
+int editorRowCxToRx(erow *row, int cx);
 
 //FILE I/O
 void editorOpen(char *filename);
@@ -84,6 +86,7 @@ void editorOpen(char *filename);
 void initEditor() {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
@@ -305,7 +308,7 @@ void editorRefreshScreen() {
   
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-                                            (E.cx - E.coloff) + 1);
+                                            (E.rx - E.coloff) + 1);
   abAppend(&ab, buf, strlen(buf));
 
   abAppend(&ab, "\x1b[?25h", 6);
@@ -352,17 +355,22 @@ void editorDrawRows(struct abuf *ab) {
 }
 
 void editorScroll() {
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+  }
+
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.coloff) {
-    E.coloff = E.cx;
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols) {
-    E.coloff = E.cx - E.screencols + 1;
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -416,6 +424,18 @@ void editorUpdateRow(erow *row) {
   row->render[idx] = '\0';
   row->rsize = idx;
 }
+
+int editorRowCxToRx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j = 0; j < cx; j++) {
+    if (row->chars[j] == '\t')
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    rx++;
+  }
+  return rx;
+}
+
 
 //FILE I/O FUNCTIONS
 void editorOpen(char *filename) {
