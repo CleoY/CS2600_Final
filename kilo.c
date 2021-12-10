382389c902961f0442035aeb10627ad538f4f1eb
CleoY <ccyau@cpp.edu>
2021-12-10 03:42:55 +0000

    Finished step 84.

--- kilo.c
+++ kilo.c
@@ -4,6 +4,7 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 #define ABUF_INIT {NULL, 0}
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
 #include <unistd.h>
 #include <termios.h>
@@ -393,12 +394,24 @@ void editorAppendRow(char *s, size_t len) {
 }
 
 void editorUpdateRow(erow *row) {
-  free(row->render);
-  row->render = malloc(row->size + 1);
+  int tabs = 0;
   int j;
+  for (j = 0; j < row->size; j++)
+    if (row->chars[j] == '\t') 
+      tabs++;
+
+  free(row->render);
+  row->render = malloc(row->size + tabs*(KILO_TAB_STOP - 1) + 1);
+  
   int idx = 0;
   for (j = 0; j < row->size; j++) {
-    row->render[idx++] = row->chars[j];
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) 
+        row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
   }
   row->render[idx] = '\0';
   row->rsize = idx;
