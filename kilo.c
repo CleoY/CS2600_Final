927637a490f453b6377af05d7c4da7228fc31d13
CleoY <ccyau@cpp.edu>
2021-12-10 09:02:30 +0000

    Finished delete functionality and step 121.

--- kilo.c
+++ kilo.c
@@ -98,6 +98,9 @@ void editorUpdateRow(erow *row);
 int editorRowCxToRx(erow *row, int cx);
 void editorRowInsertChar(erow *row, int at, int c);
 void editorRowDelChar(erow *row, int at);
+void editorFreeRow(erow *row);
+void editorDelRow(int at);
+void editorRowAppendString(erow *row, char *s, size_t len);
 
 //FILE I/O
 void editorOpen(char *filename);
@@ -497,11 +500,19 @@ void editorInsertChar(int c) {
 }
 
 void editorDelChar() {
-  if (E.cy == E.numrows) return;
+  if (E.cy == E.numrows) 
+    return;
+  if (E.cx == 0 && E.cy == 0) 
+    return;
   erow *row = &E.row[E.cy];
   if (E.cx > 0) {
     editorRowDelChar(row, E.cx - 1);
     E.cx--;
+  } else {
+    E.cx = E.row[E.cy - 1].size;
+    editorRowAppendString(&E.row[E.cy - 1], row->chars, row->size);
+    editorDelRow(E.cy);
+    E.cy--;
   }
 }
 
@@ -590,6 +601,28 @@ void editorRowDelChar(erow *row, int at) {
   if (at < 0 || at >= row->size) return;
   memmove(&row->chars[at], &row->chars[at + 1], row->size - at);
   row->size--;
+  editorUpdateRow(row);
+  E.dirty++;
+}
+
+void editorFreeRow(erow *row) {
+  free(row->render);
+  free(row->chars);
+}
+
+void editorDelRow(int at) {
+  if (at < 0 || at >= E.numrows) return;
+  editorFreeRow(&E.row[at]);
+  memmove(&E.row[at], &E.row[at + 1], sizeof(erow) * (E.numrows - at - 1));
+  E.numrows--;
+  E.dirty++;
+}
+
+void editorRowAppendString(erow *row, char *s, size_t len) {
+  row->chars = realloc(row->chars, row->size + len + 1);
+  memcpy(&row->chars[row->size], s, len);
+  row->size += len;
+  row->chars[row->size] = '\0';
   editorUpdateRow(row);
   E.dirty++;
 }
