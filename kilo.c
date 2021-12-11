e1900d62b3786df9891247b6cd34aaa633a2f10b
CleoY <ccyau@cpp.edu>
2021-12-11 00:56:49 +0000

    Added feature to highlight strings and escape double quotes. Finished step 168.

--- kilo.c
+++ kilo.c
@@ -8,6 +8,7 @@
 #define KILO_TAB_STOP 8
 #define KILO_QUIT_TIMES 3
 #define HL_HIGHLIGHT_NUMBERS (1<<0)
+#define HL_HIGHLIGHT_STRINGS (1<<1)
 #define HLDB_ENTRIES (sizeof(HLDB) / sizeof(HLDB[0]))
 
 #include <unistd.h>
@@ -37,6 +38,7 @@ enum editorKey {
 };
 enum editorHighlight {
   HL_NORMAL = 0,
+  HL_STRING,
   HL_NUMBER,
   HL_MATCH
 };
@@ -59,7 +61,7 @@ struct editorSyntax HLDB[] = {
   {
     "c",
     C_HL_extensions,
-    HL_HIGHLIGHT_NUMBERS
+    HL_HIGHLIGHT_NUMBERS | HL_HIGHLIGHT_STRINGS
   },
 };
 
@@ -933,11 +935,35 @@ void editorUpdateSyntax(erow *row) {
     return;
   
   int prev_sep = 1;
+  int in_string = 0;
   int i = 0;
   
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+    
+    if (E.syntax->flags & HL_HIGHLIGHT_STRINGS) {
+      if (in_string) {
+        row->hl[i] = HL_STRING;
+        if (c == '\\' && i + 1 < row->rsize) {
+          row->hl[i + 1] = HL_STRING;
+          i += 2;
+          continue;
+        }
+        if (c == in_string) 
+          in_string = 0;
+        i++;
+        prev_sep = 1;
+        continue;
+      } else {
+        if (c == '"' || c == '\'') {
+          in_string = c;
+          row->hl[i] = HL_STRING;
+          i++;
+          continue;
+        }
+      }
+    }
     
     if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
       if ((isdigit(c) && (prev_sep || prev_hl == HL_NUMBER)) ||
@@ -956,6 +982,8 @@ void editorUpdateSyntax(erow *row) {
 
 int editorSyntaxToColor(int hl) {
   switch (hl) {
+    case HL_STRING: 
+      return 35;
     case HL_NUMBER: 
       return 31;
     case HL_MATCH: 
